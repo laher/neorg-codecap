9dc5e188a85b6337ea1f45fb8d7674a811e71d2a
Am Laher <am@laher.net.nz>
2023-05-28 11:48:22 +1200

    code blocks

--- lua/codecap.lua
+++ lua/codecap.lua
@@ -11,6 +11,22 @@ function M.setup(config)
     end
 end
 
+
+
+function M.get_range(mode, add_current_line_on_normal_mode)
+  local lstart
+  local lend
+  if mode == "v" then
+    local pos1 = vim.fn.getpos("v")[2]
+    local pos2 = vim.fn.getcurpos()[2]
+    lstart = math.min(pos1, pos2)
+    lend = math.max(pos1, pos2)
+  elseif add_current_line_on_normal_mode == true then
+    lstart = vim.api.nvim_win_get_cursor(0)[1]
+  end
+  return { lstart = lstart, lend = lend }
+end
+
 function M.cap(mode, opts)
     vim.notify(mode .. " - " .. vim.inspect(opts))
     local ui
@@ -22,12 +38,14 @@ function M.cap(mode, opts)
     -- if gitlinker then call neorg-codecap via a callback
     local url = require("gitlinker").get_buf_range_url(mode, {
         action_callback = function(url)
-            vim.cmd(string.format("Neorg codecap %s %s", ui, url))
+            local cmd = string.format("Neorg codecap %s %s %s", ui, mode, url)
+            vim.cmd(cmd)
         end,
     })
     if not url then
         -- otherwise just use neorg-codecap
-        vim.cmd(string.format("Neorg codecap %s", ui))
+        local cmd = string.format("Neorg codecap %s %s", ui, mode)
+        vim.cmd(cmd)
     end
 end
 
--- lua/neorg/modules/external/codecap/module.lua
+++ lua/neorg/modules/external/codecap/module.lua
@@ -12,8 +12,8 @@ module.load = function()
         codecap = {
             args = 1,
             subcommands = {
-                popup = { max_args = 1, name = "codecap.popup" },
-                vsplit = { max_args = 1, name = "codecap.vsplit" },
+                noshow = { max_args = 2, name = "codecap.noshow" },
+                vsplit = { max_args = 2, name = "codecap.vsplit" },
                 inbox = { args = 0, name = "codecap.inbox" },
             },
         },
@@ -25,56 +25,86 @@ module.private = {
         local workspace = module.required["core.dirman"].get_current_workspace()[2]
         return workspace .. "/inbox.norg"
     end,
-}
 
-local function basename(path)
-    return path:sub(path:find("/[^/]*$") + 1)
-end
+    get_range = function(mode, add_current_line_on_normal_mode)
+      local lstart
+      local lend
+      if mode == "v" then
+        local pos1 = vim.fn.getpos("v")[2]
+        local pos2 = vim.fn.getcurpos()[2]
+        lstart = math.min(pos1, pos2)
+        lend = math.max(pos1, pos2)
+      elseif add_current_line_on_normal_mode == true then
+        lstart = vim.api.nvim_win_get_cursor(0)[1]
+      end
+      if lstart and not lend then
+        lend = lstart
+      end
+      return { lstart = lstart, lend = lend }
+    end,
 
-local function get_url_and_short(url)
-    if not url then
-        -- oops not a git repo. use a file
-        url = vim.api.nvim_buf_get_name(0)
-        local line = vim.api.nvim_win_get_cursor(0)
-        url = string.format("/ %s:%d", url, line[1])
-    end
-    local short
-    if #url > 30 then
-        short = string.format("%s...%s", url:sub(0, 15), url:sub(-15, #url))
-    else
-        short = url
-    end
-    return { url, short }
-end
+    basename = function(path)
+        return path:sub(path:find("/[^/]*$") + 1)
+    end,
 
-local function write_to_inbox(url, text)
-    local inbox = module.private.inbox_filename()
+    get_url_and_range = function(mode, url)
+        local range = module.private.get_range(mode, true)
+        if not url or url == '' or url == '-' then
+            -- oops not a git repo. use a file
+            url = vim.api.nvim_buf_get_name(0)
+            local line = vim.api.nvim_win_get_cursor(0)
+            url = string.format("/ %s:%d", url, line[1])
+        end
+        local short
+        if #url > 30 then
+            short = string.format("%s...%s", url:sub(0, 15), url:sub(-15, #url))
+        else
+            short = url
+        end
+        local codeblock
+        if range.lstart then
+          local lines = vim.api.nvim_buf_get_lines(0, range.lstart, range.lend, false)
+          if lines and #lines then
+            local ft = vim.bo.filetype
+            codeblock = table.concat(lines, "\n")
+            codeblock = string.format('@code %s\n%s\n@end\n', ft, codeblock)
+          end
+        end
+        -- empty string won't force a linebreak before url
+        if not codeblock then codeblock= '' end
+        return { url, short, codeblock }
+    end,
 
-    local preamble
-    local inbox_file = io.open(inbox, "r")
-    if inbox_file == nil then
+    write_to_inbox = function(url, codeblock, description)
+        local inbox = module.private.inbox_filename()
+
+        local preamble
+        local inbox_file = io.open(inbox, "r")
+        if inbox_file == nil then
         -- create a file heading
         preamble = [[* Inbox
 ]]
-    else
-        io.close(inbox_file) -- close then re-open in append mode
-        preamble = ""
-    end
-    -- new TODO with a link
-    local fname = basename(url)
-    text = preamble .. string.format(
+        else
+            io.close(inbox_file) -- close then re-open in append mode
+            preamble = ""
+        end
+        -- new TODO with a link
+        local fname = module.private.basename(url)
+        local text = preamble .. string.format(
         [[- ( ) %s
-{%s}[%s]
+%sSee {%s}[%s]
 ]],
-        text,
-        url,
-        fname
-    )
-    inbox_file = io.open(inbox, "a")
-    io.output(inbox_file)
-    io.write(text)
-    io.close(inbox_file)
-end
+            description,
+            codeblock,
+            url,
+            fname
+        )
+        inbox_file = io.open(inbox, "a")
+        io.output(inbox_file)
+        io.write(text)
+        io.close(inbox_file)
+    end
+}
 
 module.public = {
     open_inbox = function()
@@ -82,25 +112,33 @@ module.public = {
         -- move cursor?
     end,
 
-    show_capture_vsplit = function(url)
-        url, _ = unpack(get_url_and_short(url))
-        vim.notify(url)
-        write_to_inbox(url, " ")
-        vim.cmd.vs(module.private.inbox_filename())
-        -- move to penultimate line and insert-mode
-        vim.cmd("$-1")
-        vim.cmd("norm! $")
-        vim.cmd("startinsert")
+    show_capture_vsplit = function(mode, url)
+        module.public.show_capture_noshow(mode, url, function(codeblock)
+
+          -- local codeblock
+          -- url, _, codeblock = unpack(module.private.get_url_and_range(mode, url))
+          -- vim.notify(url, 'info', {title = title})
+          -- module.private.write_to_inbox(url, codeblock, "")
+          vim.cmd.vs(module.private.inbox_filename())
+          -- move to todo line and insert-mode
+          local _, count = codeblock:gsub('\n', '\n')
+          vim.cmd(string.format("$-%d", count+1))
+          vim.cmd("norm! $")
+          vim.cmd("startinsert")
+      end)
     end,
 
     -- url already supplied
-    show_capture_popup = function(url)
-        local short
-        url, short = unpack(get_url_and_short(url))
-        module.required["core.ui"].create_prompt("NeorgCapture", short .. " : ", function(text)
-            write_to_inbox(url, text)
+    show_capture_noshow = function(mode, url, callback)
+        local short, codeblock
+        url, short, codeblock = unpack(module.private.get_url_and_range(mode, url))
+        module.required["core.ui"].create_prompt("NeorgCapture", short .. " : ", function(description)
             vim.cmd("bd!") -- close this prompt
+            module.private.write_to_inbox(url, codeblock, description)
             vim.notify("Todo item added to " .. module.private.inbox_filename(), "info", { title = title })
+            if callback then
+              callback(codeblock)
+            end
         end, {
             center_x = true,
             center_y = true,
@@ -115,19 +153,22 @@ module.public = {
 }
 
 module.on_event = function(event)
-    local url
+    local mode, url
     if #event.content > 0 then
-        url = event.content[1]
+      mode = event.content[1]
+      if #event.content > 1 then
+        url = event.content[2]
+      end
     end
     if event.split_type[2] == "codecap.vsplit" then
         vim.schedule(function()
-            module.public.show_capture_vsplit(url)
+            module.public.show_capture_vsplit(mode, url)
         end)
-    elseif event.split_type[2] == "codecap.popup" then
+    elseif event.split_type[2] == "codecap.noshow" then
         --vim.notify(vim.inspect(event))
 
         vim.schedule(function()
-            module.public.show_capture_popup(url)
+            module.public.show_capture_noshow(mode, url)
         end)
     elseif event.split_type[2] == "codecap.inbox" then
         vim.schedule(module.public.open_inbox)
@@ -136,7 +177,7 @@ end
 
 module.events.subscribed = {
     ["core.neorgcmd"] = {
-        ["codecap.popup"] = true,
+        ["codecap.noshow"] = true,
         ["codecap.vsplit"] = true,
         ["codecap.inbox"] = true,
     },
